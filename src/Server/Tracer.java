69321df95180508ebab61c739087855d311b3bf8
EddiePi <piyidi36@sina.com>
2017-04-01 12:37:21 -0600

    test graphite

--- src/Server/Tracer.java
+++ src/Server/Tracer.java
@@ -285,7 +285,7 @@ public class Tracer {
     public void fetchLastApp() {
         if(needFetch) {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(2000);
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
