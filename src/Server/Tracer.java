9cf96ea24499244a0ebfd19402ee869bf77eb6f9
EddiePi <piyidi36@sina.com>
2017-04-03 10:28:22 -0600

    debugging

--- src/Server/Tracer.java
+++ src/Server/Tracer.java
@@ -279,6 +279,7 @@ public class Tracer {
         for(Task task: taskMap.values()) {
             ContainerMetrics cMetrics = containerIdToMetrics.get(task.containerId);
             if (task.taskMetrics.size() > 0) {
+                System.out.print("appId: " + task.appId + " jobId: " + task.jobId + " stageId: " + task.stageId + "\n");
                 cMetrics.appId = task.appId;
                 cMetrics.jobId = task.jobId;
                 cMetrics.stageId = task.stageId;
