f01f0a47e27938a58e65514b001e8dad17b78a0b
EddiePi <piyidi36@sina.com>
2017-04-21 17:11:39 -0600

    distributed start

--- start-all.sh
+++ start-all.sh
@@ -2,7 +2,9 @@
 
 for i in disco-00{12..19}
 do
-	ssh $i nohup /home/eddie/tracing-server/run.sh
+	ssh $i "/home/eddie/tracing-server/run.sh > /dev/null 2>&1 &"
+        pid=$(ps aux | grep "ssh -f -n $i /home/eddie/tracing-server/run.sh" | awk '{print $2}' | sort -n | head -n 1) # obtain the pid  
+        kill ${pid}
 done
 wait
 exit 0
