6047a309e088fb3d2fc617ecf16568f8bfe0966e
EddiePi <piyidi36@sina.com>
2017-05-19 16:21:19 -0600

    bug fixed

--- delete-alldata.sh
+++ delete-alldata.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-rm -rf /home/eddie/tracing-server/data
+rm -rf /home/eddie/tracedata/data
 for i in disco-00{12..19}
 do
-	ssh $i "rm -rf /home/eddie/tracing-server/data"
+	ssh $i "rm -rf /home/eddie/tracedata/data"
 done
 wait
 exit 0
